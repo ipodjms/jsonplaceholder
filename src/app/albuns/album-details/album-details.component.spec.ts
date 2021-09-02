b3e02f3d601f3273fd83fe826ea76a7034ccd410
Joao <jhoudoris@gmail.com>
2021-09-02 11:39:44 -0300

    atualizacao angular~

--- src/app/albuns/album-details/album-details.component.spec.ts
+++ src/app/albuns/album-details/album-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AlbumDetailsComponent } from './album-details.component';
 
@@ -6,7 +6,7 @@ describe('AlbumDetailsComponent', () => {
   let component: AlbumDetailsComponent;
   let fixture: ComponentFixture<AlbumDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AlbumDetailsComponent ]
     })
